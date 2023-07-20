8156b23508bb8398e4babfebfc862c976836c17f
DARKN0$Y <109553205+DARKNOSY@users.noreply.github.com>
2023-07-20 23:09:38 +0100

    Create powershell.ps1

---
+++ Example/powershell.ps1
@@ -0,0 +1,6 @@
+Write-Host "What are Michael Jackson's Pronoums?"
+Write-Host "He/Him"
+Write-Host "LMAOOO"
+Write-Host "Coded By DARKNOSY"
+Write-Host "END"
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
