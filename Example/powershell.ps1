76e85b56355493a41d585cc42e5dcdc8d7088dad
D4RKN0$6 <109553205+DARKNOSY@users.noreply.github.com>
2024-02-12 22:01:17 +0100

    Update powershell.ps1

--- Example/powershell.ps1
+++ Example/powershell.ps1
@@ -1,6 +1,6 @@
 Write-Host "What are Michael Jackson's Pronoums?"
 Write-Host "He/Him"
-Write-Host "LMAOOO"
+Write-Host "LMAOOO (HELP HELP HELP I am held hostage between the 9th and 10th Streets in northwest Washington, D.C)"
 Write-Host "Coded By DARKNOSY"
 Write-Host "END"
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
